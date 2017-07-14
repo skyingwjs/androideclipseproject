70bcc2c7299a27c308bcdaba735019ce53d18275
JinshanWang <cy_wangjinshan@163.com>
2017-07-14 11:48:26 +0800

    commit BaiduVoiceSDKAndroid1.4.1

---
+++ BaiduVoiceSDKAndroid1.4.1/VoiceRecognitionDemo/src/com/baidu/android/voicedemo/Constants.java
@@ -0,0 +1,12 @@
+package com.baidu.android.voicedemo;
+
+public class Constants {
+    /*
+     * 请登录http://developer.baidu.com，注册成为百度开发者，创建应用并开通语音技术服务后，
+     * 将对应的ApiKey和SecretKey填写后再测试
+     */
+    public static final String API_KEY = "GGsqeQVIeacGsYLWYt7YAORG";
+
+    public static final String SECRET_KEY = "861ae888266b0ceedca9e6179e02839b";
+
+}
